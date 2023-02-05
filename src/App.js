579ba0b2467a26c2bef85016377f9de975d818e0
Suraj <surajbasnet449@gmail.com>
2023-02-05 14:19:28 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import "./index.css";
 
-function App() {
+const App = () => {
+  const [p, setP] = useState(0);
+  const [t, setT] = useState(0);
+  const [r, setR] = useState(0);
+  const [si, setSi] = useState(0);
+
+  const yourInterest = () => {
+    setSi((p * t * r) / 100);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="button">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+            Simple Interest Calculator
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <br></br>
+      <TextField
+        onChange={(event) => setP(event.target.value)}
+        id="outlined-basic"
+        label="Enter Principle"
+        variant="outlined"
+      />
+      <TextField
+        onChange={(event) => setT(event.target.value)}
+        id="outlined-basic"
+        label="Enter Time"
+        variant="outlined"
+      />
+      <TextField
+        onChange={(event) => setR(event.target.value)}
+        id="outlined-basic"
+        label="Enter Rate"
+        variant="outlined"
+      />
+      <br /> <br />
+      <Button onClick={() => yourInterest()} variant="contained">
+        Calculate
+      </Button>
+      <Typography variant="h6" gutterBottom>
+        Your Simple Interest is: {si}
+      </Typography>
     </div>
   );
-}
+};
 
 export default App;
